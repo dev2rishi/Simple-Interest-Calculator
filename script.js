9ad3c17029b760a5a1d0fee0470367d209ce3dcc
Rishabh Singh <86129510+dev2rishi@users.noreply.github.com>
2021-08-17 23:10:58 +0530

    All Modifications done
    SI calc app working fine

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
     
+    if(principal == "" || principal <=0)
+    {
+      alert("Enter a positive number");
+      document.getElementById("principal").focus();
+      return;
+    }
+
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate / 100;
+    
+    var year = new Date().getFullYear()+parseInt(years);
+    document.getElementById("result").innerHTML="If you deposit " + "<span class='highlight'>" +principal+ "</span>"  + ", <br> at an interest rate of "+ "<span class='highlight'>" + rate + "</span>%." + "<br> You will receive an amount of " + "<span class='highlight'>" + interest + "</span>" + ", <br> in the year " + "<span class='highlight'>" + year + "</span>";
 }
-        
+    
+
+function updateRate()
+
+{
+    var rateval = document.getElementById("rate");
+    var output = document.getElementById("rate_val");
+    output.innerHTML = rateval.value;
+ 
+   // Update the current value of slider 
+     rateval.oninput = function()
+     {
+        output.innerHTML = this.value;
+     }
+       
+}
+    
+
+
